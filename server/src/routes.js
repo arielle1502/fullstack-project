f270dd5391d9b45c88a9039ddc6b263af4d00bdd
arielle1502 <arielle1502@yahoo.com>
2020-08-20 17:27:21 +1000

    added another user
    test

--- server/src/routes.js
+++ server/src/routes.js
@@ -2,10 +2,6 @@ const AuthenticationController = require('./controllers/AuthenticationController
 
 module.exports = (app) => {
 
-    app.get('/status', (req, res) => {
-      res.send({message: 'hello world'});
-    });
-
     app.post('/register', AuthenticationController.register);
     
 }
