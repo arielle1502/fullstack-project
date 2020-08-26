89ea03d25d772c49ee9cfae5251727d5f04579f0
arielle1502 <arielle1502@yahoo.com>
2020-08-26 15:59:28 +1000

    added bootstrap

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,35 +1,43 @@
 <template>
-  <div class="hello">
-    <input type="email" name="email" placeholder="Email" v-model="email"/>
-    <input type="password" name="password" placeholder="Password" v-model="password"/>
+<div class="container">
+<form>
+  <div class="form-group">
+    <input class="form-control" type="email" name="email" placeholder="email" v-model="email" />
+    <input
+      class="form-control"
+      type="password"
+      name="password"
+      placeholder="password"
+      v-model="password"
+    />
     <div v-html="error" class="error"></div>
-    <button @click="register">Register</button>
-    
+    <button @click="register" class="btn btn-primary">Register</button>
+  </div>
+  </form>
   </div>
 </template>
 
 <script>
-import AuthenticationService from '@/services/AuthenticationService'
+import AuthenticationService from "@/services/AuthenticationService";
 export default {
-  name: 'Register',
+  name: "HelloWorld",
   data() {
-    return{
-      email: "tst@test.com",
+    return {
+      email: "test@test.com",
       password: "password",
-      error: null
+      error: null,
     };
   },
-  methods:{
+  methods: {
     async register() {
-      try{
-      await AuthenticationService.register(
-        {
-      email: this.email,
-      password: this.password
-    });
-      }catch (error){
-        // found the right side below by triggering an error and looking inn vue dev tools and seeing the output
-        this.error = error.response.data.error
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password,
+        });
+      } catch (error) {
+        console.log(error);
+        this.error = error.response.data.error;
       }
     },
   },
@@ -38,8 +46,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.error{
-  color:red;
+.error {
+  color: red;
 }
 
 h3 {
@@ -56,4 +64,5 @@ li {
 a {
   color: #42b983;
 }
+
 </style>
